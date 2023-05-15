3a8d45d582a13928931c99623ed7753c7d73be24
YeondongChoe <chldusehd789@gmail.com>
2023-05-15 16:55:02 +0900

    render bug fix

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { styled, createGlobalStyle} from "styled-components";
+import { styled } from "styled-components";
 import Header from "./Header";
 import Footer from "./Footer";
 
-const GlobalStyle = createGlobalStyle`
-  box-sizing: border-box;
-  margin: 0px;
-  padding: 0px;
-  border: 0px;
-`
 function App() {
   return (
     <>
-    <GlobalStyle/>
-      <Header />
-      <Footer />
+      <Header/>
+      <Footer/>
     </>
   );
 }
